3126264707000eb7a1c1f84a0a20517d7c7f6ce0
Gwenn Durpoix-Espinasson <durpoixgwenn@gmail.com>
2025-05-13 22:10:25 +0200

    working api query

--- nextSub.py
+++ nextSub.py
@@ -1,5 +1,5 @@
 import requests
-
+import os
 
 PRIM_URL = "https://prim.iledefrance-mobilites.fr/marketplace/stop-monitoring"
 LINE_13_CODE = "C01383"
@@ -11,18 +11,17 @@ def get_next_sub():
     """
 
     url = PRIM_URL
-    payload = {'MonitoringRef': STOP_GUY_MOQUET_CODE,
-               'LineRef': LINE_13_CODE
+    params = {'MonitoringRef': f'STIF:StopPoint:Q:{STOP_GUY_MOQUET_CODE}:',
+               'LineRef': f'STIF:Line::{LINE_13_CODE}:'
                }
     
     headers = {
-        'apiKey': SUBWAY_API_KEY
+        'apiKey': os.environ['SUBWAY_API_KEY']
         }
 
-    response = requests.request("GET", url, headers=headers, data=payload)
+    response = requests.request("GET", url, headers=headers, params=params,)
 
     print(response.text)
 
 if __name__ == "__main__":
     get_next_sub()
-    
