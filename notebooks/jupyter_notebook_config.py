b5f0c73bf12149c213f7b1fefa6ac118d23dbd7d
Gwenn Durpoix-Espinasson <durpoixgwenn@gmail.com>
2025-05-16 00:10:35 +0200

    notebooks set up and backed up

---
+++ notebooks/jupyter_notebook_config.py
@@ -0,0 +1,3 @@
+c.NotebookApp.password = 'argon2:$argon2id$v=19$m=10240,t=10,p=8$i+gS5KuBM4OIxnJFX0qAcQ$CkDkqaW3Jlluz2uteRfDj98lMwkTpNVP/ZdizWAcCxM'
+c.NotebookApp.open_browser = True
+c.NotebookApp.port = 8888
